dcd891cd64cef170facc378bf33043a48120673f
clara <clara@clara.com>
2017-09-24 18:26:58 -0500

    Figure out average time for lake; test case-insensitive

--- src/main/java/week_4/Question_5_Lake_Running.java
+++ src/main/java/week_4/Question_5_Lake_Running.java
@@ -6,6 +6,7 @@ import static input.InputUtils.*;
  *
  You are a runner, and you are in training for a race. You'd like to keep track of all of your
  times for your training runs. You only like to run around lakes. Here's some example data,
+ 
  Cedar, 45.15
  Cedar, 43.32
  Harriet, 49.34
@@ -31,8 +32,12 @@ import static input.InputUtils.*;
  Harriet, 44.43
  Como, 28.14
 
- Don't just store the fastest time, because you may want to add more analysis in the future
- (e.g. figuring out average time).
+ You should also be able to calculate the average time. So, for the same data as above,
+ your program will display something like this (numbers are truncated with 2 decimal places)
+ 
+ Cedar, 44.23
+ Harriet, 46.66
+ Como, 30.12
  
  Your program should be case-insensitive. So "Como" is the same lake as "como" or "COMO".
 
@@ -60,22 +65,28 @@ public class Question_5_Lake_Running {
             double time = doubleInput("Enter time for running lake " + name);
             addLake(name, time);
         }
-
-        printFastestTimeForAllLakes();
+    
+        printReportForAllLakes();
 
     }
 
 
 
 
-    public void printFastestTimeForAllLakes() {
-        // TODO read data from your data structure, and print the fastest time for each lake.
-        // Your output should look something like this
-
-        // Lake Harriet, 50.12 minutes
-        // Como Lake, 45.12 minutes
-        // ....
-        // Use the fastestTimeForLake method to help.
+    public void printReportForAllLakes() {
+        
+        // TODO read data from your data structure, and print the average and fastest time for each lake.
+        /* Your output should look something like this, with the average, followed by the fastest.
+    
+        Lake       Average      Fastest
+        Cedar      44.23        43.32
+        Harriet    46.66        44.43
+        Como       30.12        28.12
+        
+        
+        */
+        
+        // Use the fastestTimeForLake method, and averageTimeForLake method to help.
 
 
     }
@@ -91,8 +102,21 @@ public class Question_5_Lake_Running {
         return 0; // replace with your code
 
     }
-
-
+    
+    
+    public double averageTimeForLake(String lakeName) {
+        // TODO read data from your data structure, and find the average time for the lake of this name.
+        
+        // Your lake name should not be case sensitive. "Como" is the same as "como".
+        
+        // If the lake is not in your data structure, return -1 to indicate it was not found.
+        
+        return 0; // replace with your code
+        
+    }
+    
+    
+    
     public void addLake(String name, double time) {
         // TODO Add the data for this lake to your data structure
         // Your lake name should not be case sensitive. "Como" is the same as "como".
--- src/test/java/week_4/Question_5_Lake_RunningTest.java
+++ src/test/java/week_4/Question_5_Lake_RunningTest.java
@@ -11,37 +11,48 @@ import static org.junit.Assert.*;
 public class Question_5_Lake_RunningTest {
     
     @Test
-    public void testPrintFastestTimeForAllLakes() throws Exception {
+    public void testPrintFastestAndAverageTimeForAllLakes() throws Exception {
 
         Question_5_Lake_Running q5 = new Question_5_Lake_Running();
 
         q5.addLake("Como", 5);
-        q5.addLake("Como", 6);
-        q5.addLake("CoMo", 3);
-
+        q5.addLake("Como", 8);
+        q5.addLake("CoMo", 2);   // average 5
+        
         q5.addLake("Harriet", 5);
-        q5.addLake("HaRRiet", 16);
-
-
+        q5.addLake("HaRRiet", 17);  // Average 11
+        
         q5.addLake("Superior", 45345);
-        q5.addLake("Superior", 1121226);
+        q5.addLake("Superior", 1121229);  // Average 583287
+        
 
         PrintUtils.catchStandardOut();
 
-        q5.printFastestTimeForAllLakes();
+        q5.printReportForAllLakes();
 
         String out = PrintUtils.resetStandardOut();
         out = out.replace("\n", " ").toLowerCase();  // remove newlines
         out = out.replace("\r", "");   // Remove carriage return
 
         /* Check that out contains
-         como .... 3 ... harriet ... 5 ... superior ... 45345
+         como ....5 ..... 3 ...
+         harriet ... 11 ..... 5 ...
+         superior ... 583287..... 45345
          In any order
         */
         
-        assertTrue(Pattern.matches(".*como.*3.*", out));
-        assertTrue(Pattern.matches(".*harriet.*5.*", out));
-        assertTrue(Pattern.matches(".*superior.*45345.*", out));
+        String msg = "Ensure you print the lake name, the average time, and the fastest time, for every lake";
+    
+    
+        Pattern como = Pattern.compile(".*como.*5.*2.*", Pattern.CASE_INSENSITIVE);
+        assertTrue(msg, como.matcher(out).matches());
+    
+        Pattern harriet = Pattern.compile(".*harriet.*11.*5.*", Pattern.CASE_INSENSITIVE);
+        assertTrue(msg, harriet.matcher(out).matches());
+    
+        Pattern superior = Pattern.compile(".*superior.*583287.*45345.*", Pattern.CASE_INSENSITIVE);
+        assertTrue(msg, superior.matcher(out).matches());
+    
         
     }
 
@@ -65,14 +76,56 @@ public class Question_5_Lake_RunningTest {
 
         double delta = 0.000001;
 
-        assertEquals(3, q5.fastestTimeForLake("Lake Como"), delta);
-        assertEquals(5, q5.fastestTimeForLake("Harriet"), delta);
-        assertEquals(45345, q5.fastestTimeForLake("Superior"), delta);
+        assertEquals("If the times for Lake Como are 5, 6, 3, then 3 should be the fastest. \nThe lake name should not be case sensitive.", 3, q5.fastestTimeForLake("Lake Como"), delta);
+        assertEquals("If the times for Lake Como are 5, 6, 3, then 3 should be the fastest. \nThe lake name should not be case sensitive.", 3, q5.fastestTimeForLake("lake como"), delta);
+
+        assertEquals("If the times for Harriet are 5, 16, the fastest should be 5. \nThe lake name should not be case sensitive.", 5, q5.fastestTimeForLake("Harriet"), delta);
+        assertEquals("If the times for Harriet are 5, 16, the fastest should be 5. \nThe lake name should not be case sensitive.", 5, q5.fastestTimeForLake("HARrIET"), delta);
+    
+        assertEquals("If the times for Superior are 45345 and 1121226 then 45345 should be fastest. \nThe lake name should not be case sensitive.", 45345, q5.fastestTimeForLake("SUPERIOR"), delta);
+        assertEquals("If the times for Superior are 45345 and 1121226 then 45345 should be fastest. \nThe lake name should not be case sensitive.", 45345, q5.fastestTimeForLake("SuPeRiOr"), delta);
+    
         assertEquals("This method should return -1 if a lake is not found", -1, q5.fastestTimeForLake("Not There"), delta);
 
     }
-
-
+    
+    
+    @Test
+    public void testAverageTimeForLake() throws Exception {
+        
+        
+        Question_5_Lake_Running q5 = new Question_5_Lake_Running();
+    
+    
+        q5.addLake("Como", 5);
+        q5.addLake("Como", 8);
+        q5.addLake("CoMo", 2);   // average 5
+    
+        q5.addLake("Harriet", 5);
+        q5.addLake("HaRRiet", 17);  // Average 11
+    
+        q5.addLake("Superior", 45345);
+        q5.addLake("Superior", 1121229);  // Average 583287
+    
+    
+        double delta = 0.1;
+        
+        assertEquals("For times 5, 8, 2, the average should be 5. \nThe lake name should not be case sensitive. ", 5, q5.averageTimeForLake("Lake Como"), delta);
+        assertEquals("For times 5, 8, 2, the average should be 5. The lake name should not be case sensitive.", 5, q5.averageTimeForLake("Lake Como"), delta);
+        
+        assertEquals("For times 5, 17, the average should be 11. The lake name should not be case sensitive.", 11, q5.averageTimeForLake("Harriet"), delta);
+        assertEquals("For times 5, 17, the average should be 11. The lake name should not be case sensitive.", 11, q5.averageTimeForLake("Harriet"), delta);
+        
+        assertEquals("For times 45345, 1121229, the average should be 583287. The lake name should not be case sensitive.", 583287, q5.averageTimeForLake("Superior"), delta);
+        assertEquals("For times 45345, 1121229, the average should be 583287. The lake name should not be case sensitive.", 583287, q5.averageTimeForLake("Superior"), delta);
+        
+        
+        assertEquals("This method should return -1 if a lake is not found", -1, q5.fastestTimeForLake("Not There"), delta);
+        
+    }
+    
+    
+    
     @Test
     public void testAddLake() throws Exception {
 
